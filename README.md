fcea20f289c580e49410a6cbf8e7b4d96972cca3
david.corbetta <david.corbetta@matera.com>
2022-04-08 18:17:08 -0300

    Update

---
+++ README.md
@@ -0,0 +1,22 @@
+# Shortcuts to spotify media using linux mind.
+
+## Shortcuts available to add on the keyboard in *"custom shortcuts"*:
+
+**Play:**  
+`run.sh "Play"`  
+**Stop:**  
+`run.sh "Pause"`  
+**Next:**  
+`run.sh "Next"`  
+**Previous:**  
+`run.sh "Previous"`  
+**Volume +5%:**  
+`run.sh "+"`  
+**Volume -5%:**  
+`run.sh "+"`
+
+**Configure notification in run.sh:**  
+`notification=true|false` 
+
+Used:
+https://specifications.freedesktop.org/mpris-spec/2.2/Player_Interface.html
--- run.sh
+++ run.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
+notification=true
 
-# Volume
+sendNotify () {
+  [[ $notification == true ]] && notify-send -i spotify "$1" "$2"
+}
+
+sendCommand () {
+  dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.$1
+  sleep 0.2
+
+  #album=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "album"|egrep -v "album"|egrep -v "array"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+  artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
+  title=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+
+  sendNotify "${artist}: ${title}" "$1" 
+}
+
+
+
 if [ $1 = "-" ] || [ $1 = "+" ]; then
+  # Volume control
   playback_input=`pactl list sink-inputs short | awk '{print $1}' | head -1` && pactl set-sink-input-volume $playback_input $15%
   if [ $1 = "-" ]; then
-    notify-send --urgency=low -i "audio-volume-low" "Volume"
+    sendNotify "Volume +5%"
   else
-    notify-send --urgency=low -i "audio-volume-high" "Volume"
+    sendNotify "Volume -5%" 
   fi
-fi
-
-# next
-if [ $1 = ">>" ]; then
-    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
-fi
-
-# Previous
-if [ $1 = "<<" ]; then
-    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
-fi
-
-# Play
-if [ $1 = ">" ]; then
-    notify-send --urgency=low -i "media-playback-start" "Play"
-    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
-fi
-
-# Pause
-if [ $1 = "||" ]; then
-    notify-send --urgency=low -i "media-playback-stop" "Stop"
-    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+else
+  sendCommand $1
 fi
