92c46725148c22f13d6da2027c051c114f87c44b
David Corbetta <david.corbetta@matera.com>
2022-10-17 10:14:31 -0300

    Update

--- soundDevice.sh
+++ soundDevice.sh
@@ -2,23 +2,32 @@
 
 notification=true 
 notificationTime=1
+fone="alsa_output.usb-BY_EDIFIER_EDIFIER_G2_II_GAMING_HEADSET_20190628-00.analog-stereo"
+monitor="alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_3__sink"
+mic="alsa_input.usb-BY_EDIFIER_EDIFIER_G2_II_GAMING_HEADSET_20190628-00.mono-fallback"
 
-sendNotify () {
+sendNotify() {
   title=$1
   message=$2
   [[ $notification == true ]] && notify-send -i audio-card "$title" "$message"; sleep $notificationTime 
 }
 
-if [ "$(pactl info | sed -En 's/Default Sink: (.*)/\1/p')" = "alsa_output.usb-BY_EDIFIER_EDIFIER_G2_II_GAMING_HEADSET_20190628-00.analog-stereo" ]; then
+setSink() {
+  pacmd set-default-sink $1
+  pacmd list-sink-inputs | grep index | while read line 
+  do
+    pacmd move-sink-input `echo $line | cut -f2 -d' '` $1
+  done
+}
+
+if [ "$(pactl info | sed -En 's/Default Sink: (.*)/\1/p')" = "$fone" ]; then
     sendNotify "Monitor LG 29WK600" "Microfone Edifier G2 II"
-    pactl set-default-sink "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_3__sink"
-    pacmd move-sink-input "12" "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_3__sink"
-    pactl set-default-source "alsa_input.usb-BY_EDIFIER_EDIFIER_G2_II_GAMING_HEADSET_20190628-00.mono-fallback"
+    setSink $monitor
+    pactl set-default-source "$mic"
 else
     sendNotify "Fone Edifier G2 II" "Microfone Edifier G2 II"
-    pactl set-default-sink "alsa_output.usb-BY_EDIFIER_EDIFIER_G2_II_GAMING_HEADSET_20190628-00.analog-stereo"
-    pacmd move-sink-input "12" "alsa_output.usb-BY_EDIFIER_EDIFIER_G2_II_GAMING_HEADSET_20190628-00.analog-stereo"
-    pactl set-default-source "alsa_input.usb-BY_EDIFIER_EDIFIER_G2_II_GAMING_HEADSET_20190628-00.mono-fallback"
+    setSink $fone
+    pactl set-default-source "$mic"
 fi
 
 
